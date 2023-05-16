6fb12a97669d71bebb5a55eaf3fe1a5e9ff8cf60
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-05-17 02:00:18 +0300

    Added works cards using javascript

--- js/popUp.js
+++ js/popUp.js
@@ -1,57 +1,67 @@
 // Define the project data
 const projects = [
-  {
-    name: 'Lock API',
-    description:
-      'Lock uses REST APIs to protect endpoints by using two techniques: tokens and sessions.',
-    image: './images/project_screenshots/Snapshoot Portfolio.svg',
-    technologies: ['Node.js', 'Express.js', 'JWT', 'Redis'],
-    liveLink: 'https://example.com',
-    sourceLink: 'https://github.com/ahmedeid6842/auth_session-token_',
-  },
-  {
-    name: 'Affable REST APIs',
-    description:
-      "Affable manages your Blog through REST API and you can also see others' Blogs.",
-    image: './images/project_screenshots/Snapshoot Portfolio (1).svg',
-    technologies: ['Node.js', 'TypeScript', 'Docker'],
-    liveLink: 'https://example.com',
-    sourceLink: 'https://github.com/ahmedeid6842/Blog',
-  },
+    {
+        name: 'Lock API',
+        description:
+            'Lock uses REST APIs to protect endpoints by using two techniques: tokens and sessions.',
+        image: './images/project_screenshots/Snapshoot Portfolio.svg',
+        technologies: ['Node.js', 'Express.js', 'JWT', 'Redis'],
+        liveLink: 'https://example.com',
+        sourceLink: 'https://github.com/ahmedeid6842/auth_session-token_',
+        projectCover: './images/project_screenshots/Snapshoot Portfolio.svg'
+    },
+    {
+        name: 'Affable REST APIs',
+        description:
+            "Affable manages your Blog through REST API and you can also see others' Blogs.",
+        image: './images/project_screenshots/Snapshoot Portfolio (1).svg',
+        technologies: ['Node.js', 'TypeScript', 'Docker'],
+        liveLink: 'https://example.com',
+        sourceLink: 'https://github.com/ahmedeid6842/Blog',
+        projectCover: './images/project_screenshots/Snapshoot Portfolio (1).svg'
+    },
 ];
 
-// Select the popup elements
-const popup = document.querySelector('.popup');
-const popupTitle = popup.querySelector('.popup__title');
-const popupImage = popup.querySelector('.popup__image');
-const popupDescription = popup.querySelector('.popup__description');
-const popupTechnologies = popup.querySelector('.popup__technologies');
-const popupLiveLink = popup.querySelector('.popup__live-link');
-const popupSourceLink = popup.querySelector('.popup__source-link');
-const popupCloseButton = popup.querySelector('.popup__close');
+function createCard() {
+    const cardContainer = document.querySelector(".card__container");
+    for (let project of projects) {
+        let technologiesList = project.technologies.map((tech) => `<li>${tech}</li>`).join("");
 
-// Select all the project buttons
-const projectButtons = document.querySelectorAll('.card__btn');
-console.log(projectButtons);
-// Add event listeners to each project button
-projectButtons.forEach((button, index) => {
-  button.addEventListener('click', () => {
-    // Show the popup window
-    popup.classList.add('popup--open');
-    // Update the popup content with the project details
-    popupTitle.textContent = projects[index].name;
-    popupImage.style.backgroundImage = `url("${projects[index].image}")`;
-    popupDescription.textContent = projects[index].description;
-    popupTechnologies.innerHTML = projects[index].technologies
-      .map((tech) => `<li>${tech}</li>`)
-      .join('');
-    popupLiveLink.setAttribute('href', projects[index].liveLink);
-    popupSourceLink.setAttribute('href', projects[index].sourceLink);
-  });
-});
+        let card = `
+            <li class="card">
 
-// Add event listener to the popup close button
-popupCloseButton.addEventListener('click', () => {
-  // Hide the popup window
-  popup.classList.remove('popup--open');
+                <div class="card__imgbox">
+                    <img src="${project.projectCover}" alt="Lock-app"
+                     class="card__img">
+                </div>
+
+                <div class="card__details">
+                    <h3 class="card__text">${project.name}</h3>
+                    <div class="card__canopy">
+                        <div class="card__canopy-header">
+                            <h4 class="card__canopy-title">CANOPY</h4>
+                            <ul class="card__canopy-list">
+                                <li>Backed Dev</li>
+                                <li>2023</li>
+                            </ul>
+                        </div>
+                    <p class="card__canopy-text">${project.description}</p>
+                    </div>
+                    <ul class="card__list">
+                         ${technologiesList}
+                    </ul>
+                    <button
+                        class="btn btn--green car__btn">See
+                        Project</button>
+                </div>
+    </li>
+        `
+        console.log(card);
+        cardContainer.insertAdjacentHTML("afterbegin", card);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    createCard();
+
 });
