fbbb57b3a74d4cc1a4526206a633ac74a731656a
peteryeungtt@gmail.com <peteryeungtt@gmail.com>
2023-05-16 11:52:52 -0400

    Add javascript code for mobile menu and updated html menu item section

--- js/index.js
+++ js/index.js
@@ -2,39 +2,24 @@ const hamburgerButton = document.querySelector('.nav-menu');
 const mainContent = document.querySelector('.main-content');
 const closeMenu = document.querySelector('.close-menu');
 const mobile = document.querySelector('.mobile-menu');
-const portfolioAnchor = document.querySelector('.nav_list_portfolio');
-const aboutAnchor = document.querySelector('.nav_list_contact');
-const contactAnchor = document.querySelector('.nav_list_about');
 
 hamburgerButton.addEventListener('click', () => {
-    hamburgerButton.classList.toggle('active');
-    mainContent.classList.toggle('inactive');
-    mobile.classList.toggle('active');
+  hamburgerButton.classList.toggle('active');
+  mainContent.classList.toggle('inactive');
+  mobile.classList.toggle('active');
 });
 
-portfolioAnchor.addEventListener('click', () => {
+const mobile_menu_anchors = document.querySelectorAll('.mobile-menu a');
+mobile_menu_anchors.forEach((anchor) => {
+  anchor.addEventListener('click', () => {
     hamburgerButton.classList.remove('active');
     mainContent.classList.remove('inactive');
     mobile.classList.remove('active');
-});
-
-aboutAnchor.addEventListener('click', () => {
-    hamburgerButton.classList.remove('active');
-    mainContent.classList.remove('inactive');
-    mobile.classList.remove('active');
-});
-
-contactAnchor.addEventListener('click', () => {
-    hamburgerButton.classList.remove('active');
-    mainContent.classList.remove('inactive');
-    mobile.classList.remove('active');
+  });
 });
 
 closeMenu.addEventListener('click', () => {
-    hamburgerButton.classList.toggle('active');
-    mainContent.classList.toggle('inactive');
-    mobile.classList.toggle('active');
+  hamburgerButton.classList.toggle('active');
+  mainContent.classList.toggle('inactive');
+  mobile.classList.toggle('active');
 });
-
-
-
