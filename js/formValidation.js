f99d2b1b528bca1b2db29e97eed6ed6c22966d0a
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-05-17 21:19:44 +0300

    Fixed some linters erros like extra lines and spaces

--- js/formValidation.js
+++ js/formValidation.js
@@ -2,12 +2,12 @@ const form = document.querySelector('.contact__form');
 const email = document.getElementById('email');
 const errorMessage = document.querySelector('.error-message');
 
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit', (event) => {
   if (email.value !== email.value.toLowerCase()) {
     event.preventDefault();
     const emailLowerCase = email.value.toLowerCase();
     email.value = emailLowerCase;
-    
+
     errorMessage.textContent = `Please enter your email in all lowercase. We've converted it to ${emailLowerCase} for you.`;
   } else {
     errorMessage.textContent = '';
