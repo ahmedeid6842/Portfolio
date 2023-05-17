539c95a21e783fe5205253cd126a7e1c3b13a3c0
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-05-17 14:40:51 +0300

    Added PopUp for project details

--- js/popUp.js
+++ js/popUp.js
@@ -1,30 +1,104 @@
 // Define the project data
 const projects = [
     {
-        name: 'Lock API',
+        name: 'Lock',
         description:
             'Lock uses REST APIs to protect endpoints by using two techniques: tokens and sessions.',
         image: './images/project_screenshots/Snapshoot Portfolio.svg',
         technologies: ['Node.js', 'Express.js', 'JWT', 'Redis'],
         liveLink: 'https://example.com',
         sourceLink: 'https://github.com/ahmedeid6842/auth_session-token_',
-        projectCover: './images/project_screenshots/Snapshoot Portfolio.svg'
+        projectCover: './images/project_screenshots/Snapshoot Portfolio.svg',
+        fullDescription: `
+            Lock uses REST APIs to protect endpoints by using two techniques: tokens and sessions.
+            The session is connected to the Redis and Mongo databases. 
+            The token is linked to MongoDB.
+        `,
+        canopy: ["Backend DEV", 2023]
     },
     {
-        name: 'Affable REST APIs',
+        name: 'Affable',
         description:
             "Affable manages your Blog through REST API and you can also see others' Blogs.",
         image: './images/project_screenshots/Snapshoot Portfolio (1).svg',
         technologies: ['Node.js', 'TypeScript', 'Docker'],
         liveLink: 'https://example.com',
         sourceLink: 'https://github.com/ahmedeid6842/Blog',
-        projectCover: './images/project_screenshots/Snapshoot Portfolio (1).svg'
+        projectCover: './images/project_screenshots/Snapshoot Portfolio (1).svg',
+        fullDescription: `
+            Affable is a powerful platform that allows you to manage your blog with
+            ease and efficiency through a RESTful API.
+        `,
+        canopy: ["Backend DEV", 2022]
     },
 ];
 
+
+function openModal(ind) {
+    const section = document.querySelector(".work");
+    const overlay = document.querySelector(".overlay");
+    let technologiesList = projects[ind].technologies.map((tech) => `<li>${tech}</li>`).join("");
+    let canopyList = projects[ind].canopy.map((elem) => `<li>${elem}</li>`).join("");
+
+    let popupHtml = `
+        <div class="popup">
+
+            <h3 class="popup__name">${projects[ind].name}</h3>
+            <button type="button" class="popup__exit">
+                <img src="images/icons8-close_black.svg" alt="">
+            </button>
+
+            <div class="card__canopy-header popup__canopy">
+                <h4 class="card__canopy-title">CANOPY</h4>
+                <ul class="card__canopy-list popup__canopy-list">
+                    ${canopyList}
+                </ul>
+            </div>
+
+            <img src="./images/desktop/Snapshoot Portfolio.svg" alt="" class="popup__img">
+
+            <div class="popup__details">
+                <p class="popup__description">
+                    ${projects[ind].fullDescription}
+                </p>
+
+                <div class="popup__tech-buttons">
+                    <ul class="card__list popup__tech">
+                        ${technologiesList}
+                    </ul>
+                    <div class="gray-line"></div>
+                    <div class="popup__buttons">
+                        <a href="${projects[ind].liveLink}" class="btn btn--green" target="_blank">See Live
+                                <img src="images/see-live-icon.svg" alt=""></a>
+                        <a href="${projects[ind].sourceLink}" class="btn btn--green" target="_blank">See Source 
+                                <img src="images/see-source-icon.svg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+
+    section.insertAdjacentHTML("afterbegin", popupHtml);
+    overlay.classList.remove("hidden");
+
+    const closeModalIcon = document.querySelector(".popup__exit");
+    const popup = document.querySelector(".popup");
+    const body = document.querySelector("body");
+
+    body.classList.add("popup-open");
+
+    closeModalIcon.addEventListener("click", () => {
+        popup.classList.add("hidden");
+        overlay.classList.add("hidden");
+        body.classList.remove("popup-open");
+    });
+}
+
+
 function createCard() {
     const cardContainer = document.querySelector(".card__container");
-    for (let project of projects) {
+    for (let index = projects.length - 1; index >= 0; index--) {
+        let project = projects[index];
         let technologiesList = project.technologies.map((tech) => `<li>${tech}</li>`).join("");
 
         let card = `
@@ -56,12 +130,16 @@ function createCard() {
                 </div>
     </li>
         `
-        console.log(card);
         cardContainer.insertAdjacentHTML("afterbegin", card);
     }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     createCard();
-
+    document.querySelectorAll(".work .btn").forEach((btn, ind) => {
+        console.log(projects[ind]);
+        btn.addEventListener("click", () => {
+            openModal(ind)
+        });
+    });
 });
